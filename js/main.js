6dd13fb99eb5894025b9fc59f86d6067d22398db
Artem <arteman8112@gmail.com>
2022-09-30 14:16:50 +0300

    Update/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomArbitrary (min, max) {
+function getRandomArbitrary(min, max) {
   if (min >= 0 && max >= 0) {
     max = Math.ceil(max);
     min = Math.floor(min);
@@ -6,13 +6,13 @@ function getRandomArbitrary (min, max) {
   } return NaN;
 }
 
-function checkLength (string,number) {
-  let numberString = string.length;
+function checkLength(string, number) {
+  const numberString = string.length;
   if (numberString <= number) {
-    return console.log(`${string},длина строки : ${string.length}`);
+    return true;
   }
-  return false
+  return false;
 }
 
-console.log(getRandomArbitrary(0,34));
+console.log(getRandomArbitrary(0, 34));
 console.log(checkLength('Привет мир', 11));
