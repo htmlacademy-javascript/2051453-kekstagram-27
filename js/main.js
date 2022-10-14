81f91b0e33a7d687045c5d49656769ca1a665429
Artem <arteman8112@gmail.com>
2022-10-14 21:33:53 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
+// Объявление констант массивов
+
+const NAMES = [
+  'Олег',
+  'Руслан',
+  'Игорь',
+  'Артем',
+  'Петр',
+  'Вячеслав',
+  'Станислав',
+  'Виктор',
+  'Владислав',
+  'Артур',
+  'Даниил',
+  'Сергей',
+  'Матвей',
+  'Кирилл',
+  'Денис',
+  'Карэн'
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+]
+
+const DESCRIPTIONS = [
+  'Красивая фотография',
+  'Хорошее качество',
+  'Прикольный дизайн',
+  'Обычная картинка',
+  'Много цветов',
+]
+
+// Проверка длины строки
+
 function checkStringLength(string, length) {
   return string.length <= length;
 }
+
+// Создание рандомного числа
 
 function getRandomPositiveInteger(a, b) {
   if (a < 0 || b < 0) {
@@ -12,81 +54,39 @@ function getRandomPositiveInteger(a, b) {
   return Math.floor(result);
 }
 
-function createName() {
-  const Names = [
-    'Олег',
-    'Руслан',
-    'Игорь',
-    'Артем',
-    'Петр',
-    'Вячеслав',
-    'Станислав',
-    'Виктор',
-    'Владислав',
-    'Артур',
-    'Даниил',
-    'Сергей',
-    'Матвей',
-    'Кирилл',
-    'Денис',
-    'Карэн'
-  ];
-  const name = Names[getRandomPositiveInteger(0, Names.length - 1)];
-  return name;
+// Создание рандомного ключа блока
+
+function createKey(array) {
+  return array[getRandomPositiveInteger(0, array.length - 1)];
 }
 
-function createMessage() {
-  const Messages = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  const message = Messages[getRandomPositiveInteger(0, Messages.length - 1)];
-  return message;
-}
-
-function createDescription() {
-  const Descriptions = [
-    'Красивая фотография',
-    'Хорошее качество',
-    'Прикольный дизайн',
-    'Обычная картинка',
-    'Много цветов',
-  ]
-  const description = Descriptions[getRandomPositiveInteger(0, Descriptions.length - 1)];
-  return description;
-}
+// Созадние комментария к объекту из 4 ключей
 
 function createComments() {
-  const idRandom = getRandomPositiveInteger(0, 1000);
-  const idAvatar = getRandomPositiveInteger(1, 6);
-  return {
-    id: idRandom,
-    avatar: 'img/avatar-' + idAvatar + '.svg',
-    message: createMessage(),
-    name: createName()
-  };
+  return [{
+    id: getRandomPositiveInteger(0, 1000),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: createKey(MESSAGES),
+    name: createKey(NAMES)
+  }];
 }
 
+// Создание полного объекта пользователя
 
-function createInfo() {
+function createInfo(number) {
   return {
-    id: getRandomPositiveInteger(1, 25),
-    url: `photos/${getRandomPositiveInteger(1, 6)}.jpg`,
-    description: createDescription(),
+    id: number,
+    url: `photos/${number}.jpg`,
+    description: createKey(DESCRIPTIONS),
     likes: getRandomPositiveInteger(15, 200),
     comments: createComments(),
   };
 }
 
-function createData(number) {
-  for (let i = 1; i <= number; i++) {
-    createInfo();
+function createData() {
+  for (let i = 1; i <= 25; i++) {
     console.log(createInfo());
   }
 }
 
-console.log(createData(55));
+console.log(createData())
